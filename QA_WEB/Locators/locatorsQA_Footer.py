902473e02b58ad7e0cd76dc724701402b4af2461
fike <werkinehfikadu@gmail.com>
2023-02-22 19:07:05 +0200

    Add footer to the qa web

---
+++ QA_WEB/Locators/locatorsQA_Footer.py
@@ -0,0 +1,42 @@
+class Locators_Footer:
+    TITLE1 = "trado"
+    FACEBOOK_LINK_PATH = "//a[contains(text(),'Facebook')]"
+    INSTAGRAM_LINK_PATH = "//a[contains(text(),'Instagram')]"
+    TWITTER_LINK_PATH = "//a[contains(text(),'Twitter')]"
+    QUESTION_PAGE_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/a[1]"
+    SHIPMENT_PAGE_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/a[2]"
+    PAYMENT_SOLUTIONS_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/a[3]"
+    MAX_PAGE_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[2]/a[4]"
+    WHO_WE_ARE_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[1]"
+    MY_ACCOUNT_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[2]"
+    ETRADO_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[3]"
+    CONTACT_US_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[4]"
+    BUSINESS_INTERFACE_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/a[5]"
+    ACCESSIBILITY_STATEMENT_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[5]/div[1]/a[3]/font[1]/font[1]"
+    PRIVACY_POLICY_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[5]/div[1]/a[2]"
+    READ_AND_AGREE_TO_THE_TERMS_LINK_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[5]/div[1]/a[1]"
+
+    # Text
+    SHIPMENT_TEXT = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/p[1]"
+    ANY_QUESTION_TEXT = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/h4[1]"
+    MAX_TEXT = "/html[1]/body[1]/app-root[1]/app-main-layout[1]/section[1]/section[1]/app-card-page[1]/div[1]/app-card-header[1]/div[1]/div[1]"
+    ABOUT_TEXT = "/html[1]/body[1]/main[1]/div[1]/div[1]/h1[1]"
+    ACCOUNT_URL = "https://qa.trado.co.il/user/personalArea"
+    ETRADO_URL = "https://qa.trado.co.il/etrado"
+    CONTACT_URL = "https://qa.trado.co.il/contact"
+    WHO_WE_ARE_URL = 'https://qa.trado.co.il/'
+    TRADO_PRODUCTION_URL = 'https://qa.trado.co.il/product/009'
+
+    ACCESSIBILITY_URL = "https://qa.trado.co.il/info/%D7%94%D7%A6%D7%94%D7%A8%D7%AA%20%D7%A0%D7%92%D7%99%D7%A9%D7%95%D7%AA"
+    PRIVACY_POLICY_TEXT = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/h1[1]/font[1]"
+    AGREE_TERM_URL = "https://qa.trado.co.il/info/%D7%AA%D7%A7%D7%A0%D7%95%D7%9F"
+
+    # Who we are Page
+    TO_TREADING_AREA_PATH = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]"
+    NEW_ORDER_BTN = "/html[1]/body[1]/main[1]/div[4]/div[1]/div[1]/div[1]/div[1]/a[1]/font[1]/font[1]"
+    CONTACT_BTN = "/html[1]/body[1]/main[1]/div[4]/div[1]/div[1]/div[1]/div[1]/a[2]/font[1]/font[1]"
+
+
+    # ---------------------------------CSS SELECTOR XPATH -------------------------------------------------------
+    FOTTER_CSS = "div:nth-child(1) div:nth-child(1) div.pages_pages div.footer_footerWrapper:nth-child(4) > div.footer_linkWrapper"
+
